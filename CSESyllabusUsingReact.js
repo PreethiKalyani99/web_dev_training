87ea948ef20ceaafc7ef0972e1bf7fbebc0725fe
preethi <fabreztina@gmail.com>
2021-08-02 21:17:08 +0530

    adding CSESyllabusUsingReact.js file

--- CSESyllabusUsingReact.js
+++ CSESyllabusUsingReact.js
@@ -1,26 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './myFirstReact.css';
 
-function Syllabus(){
-  const [semester, setSemester] = useState([]);
-  const [sem, setSem] = useState("");
+function Subject() {
+  const [subject, setSubject] = useState('');
+  const [subjects, setSubjects] = useState([]);
 
-  function handleChange(e){
-   setSem(e.target.value);
+  const handleChange = (e) => {
+    setSubject([e.target.value])
   }
-
-  function handleSemester(){
-    setSemester([...semester, sem]);
+  const addSubject = () => {
+    setSubjects([...subjects, subject])
   }
-
   
-  return (<div>
-    <input type="text" placeholder="Semester name" onChange={handleChange}></input>
-    <button onClick={handleSemester} className="semester">Add Semester</button>
-    {semester.map((sem,i)=>{
-      return <li id={i}>{sem}</li>
-    })}
-    
-  </div>)
+  return (
+    <div >
+      <ul>
+      <input type="text" placeholder="Subject Name" onChange={handleChange} value={subject} className = "subject"/>
+      <button onClick={addSubject} className="button">ADD</button> 
+      </ul>
+      <ul> 
+      { subjects.map((sub, i)=>{ return(<li key={i}>{sub}</li>)}) }
+     </ul>
+    </div>
+  )
+}
+
+function Semester() {
+  const semesters = ['Semester 1', 'Semester 2', 'Semester 3', 'Semester 4', 'Semester 5', 'Semester 6','Semester 7', 'Semester 8']
+  return (
+    <ul>
+      {semesters.map((sem,i) => {
+        return (
+          <li id={i}>
+              {sem}
+              <li>
+              <Subject />  
+              </li>
+          </li>
+        )
+      })
+      }
+    </ul>
+  )
+}
+
+function Syllabus() {
+  return (
+    <div >
+      <Semester />
+    </div>
+  );
 }
 export default Syllabus;
